4f1be31ae2d279ac086457bf41c5b53319b40500
Arunsamuvel <arun15ee03@gmail.com>
2024-07-23 22:31:26 +0530

    Finished the Tip calculator project

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
 // calcultor
 
+// selecting the input
 const display = document.getElementById("display");
+
+// this function will append the input value to the display input
 
 function appendToDisplay(input) {
   display.value += input;
 }
 
+// this function will clear the all the values in the display area
 function clearDisplay() {
   display.value = "";
 }
+
+// this function give equal value must be validate and give the result
+
 function calculate() {
   try {
     display.value = eval(display.value);
